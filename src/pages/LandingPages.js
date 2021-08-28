d117fbfd420f3622c568fb08490c648adb91c177
ayuda-darmawan <ayuda.darmawan.y@gmail.com>
2021-08-28 18:46:08 +0700

    commit

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
-import moduleName from 'elements/Button';
+import Header from "parts/Header";
+
 export default class LandingPages extends Component {
     render() {
         return (
             <div>
-
+                <Header {...this.props}></Header>
             </div>
         )
     }
