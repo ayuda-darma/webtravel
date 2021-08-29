4320178279ce3f7ca520a331ffd7cf7ce7a40b2a
ayuda-darmawan <ayuda.darmawan.y@gmail.com>
2021-08-29 20:13:27 +0700

    commit

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,15 +4,26 @@ import Header from "parts/Header";
 import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 export default class LandingPages extends Component {
+
+    constructor(props) {
+        super(props);
+        this.refMostPicked = React.createRef();
+    }
+
     render() {
         console.log(this.props);
         return (
             <div>
                 <Header {...this.props}></Header>
-                <Hero data={landingPage.hero} />
-                <MostPicked data={landingPage.mostPicked} />
+                <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+                <MostPicked
+                    refMostPicked={this.refMostPicked}
+                    data={landingPage.mostPicked}
+                />
+                <Categories data={landingPage.category} />
             </div>
         )
     }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -3,13 +3,38 @@ import Button from 'elements/Button'
 
 export default function MostPicked(props) {
     return (
-        <section className="container">
+        <section className="container" ref={props.refMostPicked}>
             <h4 className="mb-4">Most Picked</h4>
             <div className="container-grid">
                 {
                     props.data.map((item, index) => {
-                        return <div className={`item column-4${index === 0 ? " row-2" : " row-1"}`}>
-                            <div className="card">Card {index + 1}</div>
+                        return <div key={`mostpicked-${index}`}
+                            className={`item column-4${index === 0 ? " row-2" : " row-1"}`}>
+                            <div className="card card-featured">
+                                <div className="tag">
+                                    ${item.price}
+                                    <span className="font-weight-light">per {item.unit}</span>
+                                </div>
+                                <figure className="img-wrapper">
+                                    <img
+                                        src={item.imageUrl}
+                                        alt={item.name}
+                                        className="img-cover"
+                                    />
+                                </figure>
+                                <div className="meta-wrapper">
+                                    <Button
+                                        type="link"
+                                        className="stretched-link d-block text-white"
+                                        href={`/properties/${item._id}`}
+                                    >
+                                        <h5>{item.name}</h5>
+                                    </Button>
+                                    <span>
+                                        {item.city}, {item.country}
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     })
                 }
